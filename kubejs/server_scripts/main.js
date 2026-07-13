44c71af7e542c2e37f16f2c59e4025e17b0b8fd2
syarichka2-gif <syarichka2@gmail.com>
2026-07-14 00:43:30 +0300

    2

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -1,3 +1,14 @@
-// Visit the wiki for more info - https://kubejs.com/
-console.info('Hello, World! (Loaded server example script)')
+// Mystic Tales RPG - Waystones progression
+ServerEvents.recipes(event => {
+  const remove = [
+    'waystones:waystone',
+    'waystones:warp_plate',
+    'waystones:warp_scroll',
+    'waystones:return_scroll',
+    'waystones:bound_scroll'
+  ];
 
+  remove.forEach(id => event.remove({id}));
+
+  // Custom recipes will be added in the next patch.
+});
